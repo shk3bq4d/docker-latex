205854e186a58e674f0b56a75629d93a4da0c08c
Jeff Malone <123r4p+shK3Bq4d@gmail.com>
2019-01-19 08:18:17 +0100

    adds support for error handling

--- process.bash
+++ process.bash
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
 
+set -e
+
 cd /tmp
 
 jobname=`uuidgen`
 
 cat /dev/fd/0>/tmp/$jobname.latex
 
-pdflatex --interaction=nonstopmode /tmp/$jobname.latex &>/dev/null
-pdflatex --interaction=nonstopmode /tmp/$jobname.latex &>/dev/null
-
-cat /tmp/$jobname.pdf
+if pdflatex --interaction=nonstopmode /tmp/$jobname.latex >/tmp/1 2>/tmp/2; then
+	pdflatex --interaction=nonstopmode /tmp/$jobname.latex &>/dev/null
+	cat /tmp/$jobname.pdf
+	exit 0
+else
+	cat /tmp/1
+	>&2 cat /tmp/2
+	exit 1
+fi
