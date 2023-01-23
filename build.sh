974286c5355da65d6b676ad75a030afc478b7db1
Jeff Malone <jeff@feb22.ly.lan>
2023-01-23 13:44:26 +0100

    build.sh check sudo needs to executer docker cmd

--- build.sh
+++ build.sh
@@ -1,1 +1,9 @@
-docker build -t mrlatex .
+#!/usr/bin/env bash
+# ex: set filetype=sh fenc=utf-8 expandtab ts=4 sw=4 :
+
+set -euo pipefail
+umask 027
+export PATH=/usr/local/sbin:/sbin:/usr/local/bin:/bin:/usr/sbin:/usr/bin:~/bin
+
+docker ps &>/dev/null && SUDO= || SUDO=sudo
+$SUDO docker build -t mrlatex .
